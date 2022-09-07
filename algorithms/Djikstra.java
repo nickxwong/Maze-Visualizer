02f324643add2290d53e60e88a7bb3614b5d091d
Nick Wong <nicolaswong01@gmail.com>
2022-09-07 12:43:42 -0700

    Add multithreading to pathfinding component

--- algorithms/Djikstra.java
+++ algorithms/Djikstra.java
@@ -1,6 +1,7 @@
 package algorithms;
 
 import grid.Cell;
+import javax.swing.SwingWorker;
 import java.util.*;
 import java.awt.Color;
 
@@ -15,43 +16,57 @@ public class Djikstra implements Algorithm {
     }
 
     public void run() {
-        // add starting point
-        int startRow = gridArray.length - 1, startCol = gridArray[0].length - 1;
-        map.put(new AbstractMap.SimpleEntry<Integer, Integer>(startRow, startCol), 0);
-        calculateDistance(startRow, startCol);
+        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+
+            @Override
+            protected Void doInBackground() throws Exception {
+                int startRow = gridArray.length - 1, startCol = gridArray[0].length - 1;
+                map.put(new AbstractMap.SimpleEntry<Integer, Integer>(startRow, startCol), 0);
+                calculateDistance(startRow, startCol);
+                return null;
+            }
+
+        };
+        worker.execute();
     }
 
     public void calculateDistance(int curRow, int curCol) {
-        // due to the maze generation algorithm already marking each cell as visited, Djikstra will treat "visited" cells as unvisited
-        gridArray[curRow][curCol].setVisited(false);
-        int curDistance = map.get(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol));
-        if (curRow == 0 && curCol == 0) {
-            findShortestPath(0, 0, map.get(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0)));
-        }
-        // once the algorithm has reached the endpoint, all other paths no longer need to be explored
-        if (map.containsKey(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0))) {
-            return;
-        }
-        // add distances of unvisited neighbors
-        // top neighbor
-        if (curRow > 0 && gridArray[curRow][curCol].getBorder("top") == 0 && gridArray[curRow - 1][curCol].getVisited()) {
-            map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow - 1, curCol), curDistance + 1);
-            calculateDistance(curRow - 1, curCol);
-        }
-        // left neighbor
-        if (curCol > 0 && gridArray[curRow][curCol].getBorder("left") == 0 && gridArray[curRow][curCol - 1].getVisited()) {
-            map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol - 1), curDistance + 1);
-            calculateDistance(curRow, curCol - 1);
-        }
-        // bottom neighbor
-        if (curRow < gridArray.length - 1 && gridArray[curRow][curCol].getBorder("bottom") == 0 && gridArray[curRow + 1][curCol].getVisited()) {
-            map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow + 1, curCol), curDistance + 1);
-            calculateDistance(curRow + 1, curCol);
-        }
-        // right neighbor
-        if (curCol < gridArray[0].length - 1 && gridArray[curRow][curCol].getBorder("right") == 0 && gridArray[curRow][curCol + 1].getVisited()) {
-            map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol + 1), curDistance + 1);
-            calculateDistance(curRow, curCol + 1);
+        try {
+            Thread.sleep(100);
+            // due to the maze generation algorithm already marking each cell as visited, Djikstra will treat "visited" cells as unvisited
+            gridArray[curRow][curCol].setVisited(false);
+            gridArray[curRow][curCol].setBackground(Color.blue);
+            int curDistance = map.get(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol));
+            if (curRow == 0 && curCol == 0) {
+                findShortestPath(0, 0, map.get(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0)));
+            }
+            // once the algorithm has reached the endpoint, all other paths no longer need to be explored
+            if (map.containsKey(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0))) {
+                return;
+            }
+            // add distances of unvisited neighbors
+            // top neighbor
+            if (curRow > 0 && gridArray[curRow][curCol].getBorder("top") == 0 && gridArray[curRow - 1][curCol].getVisited()) {
+                map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow - 1, curCol), curDistance + 1);
+                calculateDistance(curRow - 1, curCol);
+            }
+            // left neighbor
+            if (curCol > 0 && gridArray[curRow][curCol].getBorder("left") == 0 && gridArray[curRow][curCol - 1].getVisited()) {
+                map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol - 1), curDistance + 1);
+                calculateDistance(curRow, curCol - 1);
+            }
+            // bottom neighbor
+            if (curRow < gridArray.length - 1 && gridArray[curRow][curCol].getBorder("bottom") == 0 && gridArray[curRow + 1][curCol].getVisited()) {
+                map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow + 1, curCol), curDistance + 1);
+                calculateDistance(curRow + 1, curCol);
+            }
+            // right neighbor
+            if (curCol < gridArray[0].length - 1 && gridArray[curRow][curCol].getBorder("right") == 0 && gridArray[curRow][curCol + 1].getVisited()) {
+                map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol + 1), curDistance + 1);
+                calculateDistance(curRow, curCol + 1);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
