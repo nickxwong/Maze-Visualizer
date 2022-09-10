9494cedb1c35ed26567d981cce1fae97ddcc7d4a
Nick Wong <nicolaswong01@gmail.com>
2022-09-09 17:02:04 -0700

    Fixed bug where pathfinding algorithm would keep running after path was found

--- algorithms/Djikstra.java
+++ algorithms/Djikstra.java
@@ -12,13 +12,14 @@ public class Djikstra implements Algorithm {
     private Cell[][] gridArray;
     private Map<AbstractMap.SimpleEntry<Integer, Integer>, Integer> map;
     SwingWorker<Void, Void> worker;
+    private boolean subtaskDone;
     private boolean taskDone;
     
     public Djikstra(Grid grid) {
         this.grid = grid;
         gridArray = grid.getArray();
         map = new HashMap<>();
-        taskDone = false;
+        subtaskDone = taskDone = false;
     }
 
     public void run() {
@@ -70,30 +71,31 @@ public class Djikstra implements Algorithm {
                 }
                 int curDistance = map.get(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol));
                 if (curRow == 0 && curCol == 0) {
+                    subtaskDone = true;
                     findShortestPath(worker, 0, 0, map.get(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0)));
                 }
-                // once the algorithm has reached the endpoint, all other paths no longer need to be explored
-                if (map.containsKey(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0))) {
-                    return;
-                }
+                // // once the algorithm has reached the endpoint, all other paths no longer need to be explored
+                // if (map.containsKey(new AbstractMap.SimpleEntry<Integer, Integer>(0, 0))) {
+                //     return;
+                // }
                 // add distances of unvisited neighbors
                 // top neighbor
-                if (curRow > 0 && gridArray[curRow][curCol].getBorder("top") == 0 && gridArray[curRow - 1][curCol].getVisited()) {
+                if (!subtaskDone && curRow > 0 && gridArray[curRow][curCol].getBorder("top") == 0 && gridArray[curRow - 1][curCol].getVisited()) {
                     map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow - 1, curCol), curDistance + 1);
                     calculateDistance(worker, curRow - 1, curCol);
                 }
                 // left neighbor
-                if (curCol > 0 && gridArray[curRow][curCol].getBorder("left") == 0 && gridArray[curRow][curCol - 1].getVisited()) {
+                if (!subtaskDone && curCol > 0 && gridArray[curRow][curCol].getBorder("left") == 0 && gridArray[curRow][curCol - 1].getVisited()) {
                     map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol - 1), curDistance + 1);
                     calculateDistance(worker, curRow, curCol - 1);
                 }
                 // bottom neighbor
-                if (curRow < gridArray.length - 1 && gridArray[curRow][curCol].getBorder("bottom") == 0 && gridArray[curRow + 1][curCol].getVisited()) {
+                if (!subtaskDone && curRow < gridArray.length - 1 && gridArray[curRow][curCol].getBorder("bottom") == 0 && gridArray[curRow + 1][curCol].getVisited()) {
                     map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow + 1, curCol), curDistance + 1);
                     calculateDistance(worker, curRow + 1, curCol);
                 }
                 // right neighbor
-                if (curCol < gridArray[0].length - 1 && gridArray[curRow][curCol].getBorder("right") == 0 && gridArray[curRow][curCol + 1].getVisited()) {
+                if (!subtaskDone && curCol < gridArray[0].length - 1 && gridArray[curRow][curCol].getBorder("right") == 0 && gridArray[curRow][curCol + 1].getVisited()) {
                     map.put(new AbstractMap.SimpleEntry<Integer, Integer>(curRow, curCol + 1), curDistance + 1);
                     calculateDistance(worker, curRow, curCol + 1);
                 }    
